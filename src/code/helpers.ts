3425c4ec27076231a5ae00875bc5c999fe9e3839
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-03 23:28:44 -0800

    setup ok

---
+++ src/code/helpers.ts
@@ -0,0 +1,5 @@
+const fuckYea = (num: number): number => {
+  console.log(`you said num ${num}`);
+  return num + 1;
+};
+export default fuckYea;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import './style/style.css';
+import fuckYea from "./code/helpers";
 
+fuckYea(1);
 // function component() {
 // }
 //
