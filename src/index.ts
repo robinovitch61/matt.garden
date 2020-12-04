352e4b1fa4427293fea863c39a11cffd158d72a9
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-03 22:29:52 -0800

    lay out structure

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,6 @@
 import './style/style.css';
-const Plant = require('./img/plant.png');
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = 'yo';
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myPlant = new Image();
-  myPlant.src = Plant;
-
-  element.appendChild(myPlant);
-
-  return element;
-}
-
-document.body.appendChild(component());
+// function component() {
+// }
+//
+// document.body.appendChild(component());
