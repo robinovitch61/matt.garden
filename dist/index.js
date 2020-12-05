8042b21a927894930fbbff9ac56580ef8073e35a
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-05 12:52:34 -0800

    day text

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./style/style.scss");
-var leftButton = window.document.getElementById('left');
-var rightButton = window.document.getElementById('right');
+var GIFT_DATE = new Date('12/5/2020').getTime();
+var leftButton = document.getElementById('left');
+var rightButton = document.getElementById('right');
+var dayTitle = document.getElementById('day');
 if (leftButton) {
     leftButton.addEventListener('click', function () { return console.log('left'); });
 }
 if (rightButton) {
     rightButton.addEventListener('click', function () { return console.log('right'); });
+}
+if (dayTitle) {
+    var today = new Date().getTime();
+    var diffInDays = Math.ceil((today - GIFT_DATE) / (1000 * 60 * 60 * 24));
+    var maxDay = diffInDays > 0 ? diffInDays : 0;
+    dayTitle.innerText = "day " + maxDay.toString();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import './style/style.scss';
 
-const leftButton = window.document.getElementById('left');
-const rightButton = window.document.getElementById('right');
+// const GIFT_DATE = new Date('1/05/2021') // TODO - UNCOMMENT ME
+const GIFT_DATE = new Date('12/5/2020').getTime();
+
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+const dayTitle = document.getElementById('day');
 
 if (leftButton) {
   leftButton.addEventListener('click', () => console.log('left'));
 }
 if (rightButton) {
   rightButton.addEventListener('click', () => console.log('right'));
+}
+
+if (dayTitle) {
+  const today = new Date().getTime();
+  const diffInDays = Math.ceil((today - GIFT_DATE) / (1000 * 60 * 60 * 24));
+  const maxDay = diffInDays > 0 ? diffInDays : 0;
+  dayTitle.innerText = `day ${maxDay.toString()}`;
 }
 
