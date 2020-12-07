2ee18586a5d531684448800526a4716436648dd3
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-06 16:21:07 -0800

    change day

---
+++ src/code/helpers.ts
@@ -0,0 +1,6 @@
+
+export function oppositeVisibility(elem: HTMLElement): string {
+  return elem.style.visibility === "hidden" ? "visible" : "hidden";
+}
+
+export const dayText = (day: number): string => `day ${day.toString()}`;
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,52 @@
-import './style/style.scss';
+import "./style/style.scss";
+import {dayText, oppositeVisibility} from "./code/helpers";
 
 // const GIFT_DATE = new Date('1/05/2021') // TODO - UNCOMMENT ME
-const GIFT_DATE = new Date('12/5/2020').getTime();
+const GIFT_DATE = new Date("12/1/2020").getTime();
 
-const leftButton = document.getElementById('left');
-const rightButton = document.getElementById('right');
-const dayTitle = document.getElementById('day');
+const leftButton = document.getElementById("left");
+const rightButton = document.getElementById("right");
+const dayTitle = document.getElementById("day");
 
-if (leftButton) {
-  leftButton.addEventListener('click', () => console.log('left'));
-}
-if (rightButton) {
-  rightButton.addEventListener('click', () => console.log('right'));
+if (!leftButton || !rightButton || !dayTitle) {
+  throw Error("Garden failed to grow");
 }
 
-if (dayTitle) {
-  const today = new Date().getTime();
-  const diffInDays = Math.ceil((today - GIFT_DATE) / (1000 * 60 * 60 * 24));
-  const maxDay = diffInDays > 0 ? diffInDays : 0;
-  dayTitle.innerText = `day ${maxDay.toString()}`;
-}
+const today = new Date().getTime();
+const diffInDays = Math.ceil((today - GIFT_DATE) / (1000 * 60 * 60 * 24));
+const maxDay = diffInDays > 1 ? diffInDays : 1;
+let day = maxDay;
+dayTitle.innerText = dayText(day);
+
+leftButton.style.visibility = day === 1 ? "hidden" : "visible";
+rightButton.style.visibility = day === maxDay ? "hidden" : "visible";
+
+leftButton.addEventListener("click", () => {
+  if (day === 1) {
+    return;
+  }
+  day -= 1;
+  if (rightButton.style.visibility === "hidden") {
+    rightButton.style.visibility = oppositeVisibility(rightButton);
+  }
+  if (day === 1) {
+    leftButton.style.visibility = oppositeVisibility(leftButton);
+  }
+  dayTitle.innerText = dayText(day);
+});
+
+rightButton.addEventListener("click", () => {
+  if (day === maxDay) {
+    return;
+  }
+  day += 1;
+  if (leftButton.style.visibility === "hidden") {
+    leftButton.style.visibility = oppositeVisibility(leftButton);
+  }
+  if (day === maxDay) {
+    rightButton.style.visibility = oppositeVisibility(rightButton);
+  }
+  dayTitle.innerText = dayText(day);
+});
+
 
