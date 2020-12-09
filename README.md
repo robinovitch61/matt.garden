405bb6fd4cabebe4d89bb86b4b736c61199998b8
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-08 20:51:03 -0800

    add browsersync

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ v12.19.1
 
 * ran dev server with `npm run dev`
 * run ngrok with `ngrok http 8080 -host-header="localhost:8080"` and visit http site on mobile for mobile dev
+* added browsersync webpack plugin - ngrok no longer needed on same network
 * set eslint to --fix on save in intellij
 * builds on pre-commit and adds `dist` contents to commit before committing
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,55 @@
-const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require("path");
+
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        host: "localhost",
+        port: 8090,
+        proxy: "http://localhost:8080",
+      },
+      {
+        reload: false,
+      },
+    ),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    publicPath: '/dist',
+    publicPath: "/dist",
     watchContentBase: true,
-    compress: true
-  }
+    compress: true,
+  },
 };
