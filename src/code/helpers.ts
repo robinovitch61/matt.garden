32d902d28bda1efde70d6dad2bbe15834fe1992b
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-12 00:40:23 -0800

    pad day text

--- src/code/helpers.ts
+++ src/code/helpers.ts
@@ -3,4 +3,4 @@ export function oppositeVisibility(elem: HTMLElement): string {
   return elem.style.visibility === "hidden" ? "visible" : "hidden";
 }
 
-export const dayText = (day: number): string => `day ${day.toString()}`;
+export const dayText = (day: number): string => `day ${day.toString().padStart(2, "0")}`;
